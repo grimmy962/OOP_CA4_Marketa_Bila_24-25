026818c26fcf776ab1ce175c6596bfda5c371d41
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2025-02-10 21:33:06 +0000

    Prepared project
    - made the database in phpMyAdmin
    - set up all the classes and put them in their right package
    - rewrote the pom.xml from previous exercises

--- src/main/java/org/example/BusinessObjects/AppMain.java
+++ src/main/java/org/example/BusinessObjects/AppMain.java
@@ -1,8 +1,8 @@
-package org.example;
+package org.example.BusinessObjects;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class AppMain {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
